94e2e836a60674f4ad89567466c77dda76680d3f
Ninikhachapuridze <nkhachapuridze7@gmail.com>
2021-09-28 15:47:40 +0400

    add slick

---
+++ script.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if (scroll > 300) {
+          $(".navigation-wraper").css("background" , "#FFBF9D");
+        }
+  
+        else{
+            $(".navigation-wraper").css("background" , "#9FA1AA");  	
+        }
+    })
+  })
+
+  $(function(){
+    $('.multiple-items').slick({
+     infinite: true,
+     // რამდენი სურათი გამოჩნდეს
+     slidesToShow: 3,
+     // სლაიდის დროს რამდენი ელემნტით გადავიდეს 
+     slidesToScroll: 1,
+     // რომ გამოჩნდეს ღილაკები
+     arrows: true,
+     // რომ გამოჩნდეს ბურთულები
+     dots:true,
+     // responsiv-ის კოდი
+     responsive: [
+         {
+           breakpoint: 768,
+           settings: {
+             slidesToShow: 2,
+             slidesToScroll: 1
+           }
+         },
+         {
+           breakpoint: 480,
+           settings: {
+             slidesToShow: 1,
+             slidesToScroll: 1
+           }
+         }
+     ]
+   });    
+ });
+
