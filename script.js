6c17c7ec5d6a6e3ca0ea316c4179ca72a55be9f9
Ninikhachapuridze <nkhachapuridze7@gmail.com>
2021-10-01 21:22:41 +0400

    add toggle button

--- script.js
+++ script.js
@@ -1,44 +1,55 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        var scroll = $(window).scrollTop();
-        if (scroll > 300) {
-          $(".header-wraper").css("background" , "#FFBF9D");
-        }
-  
-        else{
-            $(".navigation-wraper").css("background" , "");  	
-        }
-    })
-  })
 
-  $(function(){
-    $('.multiple-items').slick({
-     infinite: true,
-     // რამდენი სურათი გამოჩნდეს
-     slidesToShow: 3,
-     // სლაიდის დროს რამდენი ელემნტით გადავიდეს 
-     slidesToScroll: 1,
-     // რომ გამოჩნდეს ღილაკები
-     arrows: true,
-     // რომ გამოჩნდეს ბურთულები
-     dots:true,
-     // responsiv-ის კოდი
-     responsive: [
-         {
-           breakpoint: 768,
-           settings: {
-             slidesToShow: 2,
-             slidesToScroll: 1
-           }
-         },
-         {
-           breakpoint: 480,
-           settings: {
-             slidesToShow: 1,
-             slidesToScroll: 1
-           }
-         }
-     ]
-   });    
- });
+$(document).ready(function(){
+  $(window).scroll(function(){
+      var scroll = $(window).scrollTop();
+      if (scroll > 300) {
+        $(".header-wraper").css("background" , "#FFBF9D");
+      }
+
+      else{
+          $(".navigation-wraper").css("background" , "");  	
+      }
+  })
+})
+
+$(function(){
+$('.multiple-items').slick({
+  infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    dots:true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+  ]
+});  
+});
+
+document.getElementById('send').addEventListener("click", function(){
+  alert('Message sent');
+})
+
+
+let navbarlinks = document.getElementById('navbarlinks');
+let togglebutton = document.getElementById('togglebutton');
+
+togglebutton.addEventListener('click',function() {
+  navbarlinks.classList.toggle('active');
+})
+
+
+
 
